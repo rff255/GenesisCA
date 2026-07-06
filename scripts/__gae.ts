29e08d966c390392f23189dce1457aefc4a0c38e
Rodrigo F. Figueiredo <rff255@gmail.com>
2026-07-06 15:17:53 -0400

    chore: drop stray Get Age probe temp files

--- scripts/__gae.ts
+++
@@ -1,5 +0,0 @@
-
-export { compileAgentGraph } from '../src/modeler/vpl/compiler/compile.ts';
-export { compileAgentGraphWasmForModel, isAgentGraphWasmSupported } from '../src/modeler/vpl/compiler/agentWasm/compile.ts';
-export { compileAgentGraphWebGPUForModel, isAgentGraphWebGPUSupported } from '../src/modeler/vpl/compiler/agentWebgpu/compile.ts';
-export { migrateForHarness } from '../src/dev/compileHarness.ts';
